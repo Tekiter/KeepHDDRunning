4908fc86148ac4742771338ceb1b3718e8de6618
Tekiter <tekiter8@gmail.com>
2018-02-09 00:45:16 +0900

    초기 커밋.

---
+++ KeepHDDRunning/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace KeepHDDRunning
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Drive> drivelist;
+
+        public ObservableCollection<Drive> DriveList { get => drivelist; set => drivelist = value; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            DataContext = this;
+            DriveList = new ObservableCollection<Drive>();
+            UpdateDriveList();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            StartWorking();
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDriveList();
+        }
+
+        void UpdateDriveList()
+        {
+            DriveList = new ObservableCollection<Drive>();
+
+            DriveInfo[] info = DriveInfo.GetDrives();
+            foreach (var i in info)
+            {
+                if (i.DriveType == DriveType.Fixed || i.DriveType == DriveType.Removable)
+                {
+                    Drive drive = new Drive(i.Name);
+                    DriveList.Add(drive);
+                }
+            }
+        }
+
+        void StartWorking()
+        {
+            WorkingWindow window = new WorkingWindow(List_Drives.SelectedItem as Drive, int.Parse(TextBox_Interval.Text));
+            window.Show();
+            Close();
+        }
+    }
+}
---
+++ KeepHDDRunning/WorkingWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KeepHDDRunning
+{
+    /// <summary>
+    /// WorkingWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class WorkingWindow : Window
+    {
+        public Drive SelectedDrive { get; set; }
+        public int Interval { get; set; }
+        public object DriveIcon { get => SelectedDrive.Icon; }
+        public string DriveName { get => SelectedDrive.Name; }
+
+        Thread workingThread;
+        bool isRunning = true;
+
+        public WorkingWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            workingThread = new Thread(new ThreadStart(Work));
+        }
+
+        public WorkingWindow(Drive drive, int interval) : this()
+        {
+            SelectedDrive = drive;
+            Interval = interval;
+        }
+
+        void Cancel()
+        {
+            //isRunning = false;
+            
+            workingThread.Abort();
+            MainWindow window = new MainWindow();
+            window.Show();
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Cancel();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            workingThread.Start();
+        }
+
+        void Work()
+        {
+            while (isRunning)
+            {
+                string[] path = Directory.GetDirectories(SelectedDrive.Name);
+                Debug.WriteLine("Flash!");
+                Thread.Sleep(Interval); 
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            workingThread.Abort();
+        }
+    }
+}
