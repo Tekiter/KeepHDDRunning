fd431c81350df7b3318922b45b0c64af0e834880
Blue_Ninja <geon08@daum.net>
2018-02-09 00:45:16 +0900

    초기 커밋.

---
+++ KeepHDDRunning/Drive.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Windows;
+
+namespace KeepHDDRunning
+{
+    public class Drive
+    {
+        public string Name { get; set; }
+        public object Icon
+        {
+            get
+            {
+                return Imaging.CreateBitmapSourceFromHIcon(GetLargeIcon(Name).Handle,Int32Rect.Empty,BitmapSizeOptions.FromEmptyOptions());
+            }
+        }
+
+        
+
+
+        public Drive(string name)
+        {
+            Name = name;
+        }
+
+        #region GetIcon
+        [StructLayout(LayoutKind.Sequential)]
+        private struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public IntPtr iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+        private const uint SHGFI_ICON = 0x100;
+        private const uint SHGFI_LARGEICON = 0x0;
+        private const uint SHGFI_SMALLICON = 0x1;
+
+        [DllImport("shell32.dll")]
+        private static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+        [DllImport("User32.dll")]
+        private static extern int DestroyIcon(IntPtr hIcon);
+
+        private Icon GetSmallIcon(string fileName)
+        {
+            return GetIcon(fileName, SHGFI_SMALLICON);
+        }
+
+        private Icon GetLargeIcon(string fileName)
+        {
+            return GetIcon(fileName, SHGFI_LARGEICON);
+        }
+
+        private Icon GetIcon(string fileName, uint flags)
+        {
+            SHFILEINFO shinfo = new SHFILEINFO();
+            IntPtr hImgSmall = SHGetFileInfo(fileName, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFI_ICON | flags);
+
+            Icon icon = (Icon)System.Drawing.Icon.FromHandle(shinfo.hIcon).Clone();
+            DestroyIcon(shinfo.hIcon);
+            return icon;
+        }
+        #endregion
+
+    }
+}
